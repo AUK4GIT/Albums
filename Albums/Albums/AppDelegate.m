60c6ed9136ca7c8cfe5a03db1477f4bc6ff1490f
Uday Kiran Ailapaka <uday.ailapaka@cigniti.com>
2017-05-18 20:08:59 +0530

    Bug-100 Testing GIT

--- Albums/Albums/AppDelegate.m
+++ Albums/Albums/AppDelegate.m
@@ -14,7 +14,7 @@
 
 @implementation AppDelegate
 
-
+//Testing Git
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
     // Override point for customization after application launch.
